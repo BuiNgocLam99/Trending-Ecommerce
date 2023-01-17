5e88e7fcaf583293071417bf3fce5f77c78ad962
VuaLiDon <lambn957@gmail.com>
2023-01-17 21:47:29 +0700

    Make Filter Price Using Sliderrange JS And Livewire's @this.set To Set Value Of Variable In Component In Script Tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -795,6 +795,7 @@
             <script src="{{ asset('assets/js/main.js?v=3.3') }}"></script>
             <script src="{{ asset('assets/js/shop.js?v=3.3') }}"></script>
             @livewireScripts
+            @stack('script')
         </body>
 
         </html>

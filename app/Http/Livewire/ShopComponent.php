6d72a4400c5ffbc6371d34d1847125c4622c1fb6
VuaLiDon <lambn957@gmail.com>
2023-01-18 00:53:35 +0700

    Make Wishlist Remove Product From Wishlist

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -42,6 +42,18 @@ class ShopComponent extends Component
         $this->emitTo('wish-list-icon-component', 'refreshComponent');
     }
 
+    public function removeFromWishList($product_id){
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id == $product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wish-list-icon-component', 'refreshComponent');
+                return;
+            }
+        }
+    }
+
     public function render()
     {
         if ($this->orderBy == 'Price: Low to High') {
